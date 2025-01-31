064f74f52fea1608aab25a259b104b31f17978f0
Sergei Rodin <sm6078@mail.ru>
2025-01-31 05:38:55 +0300

    update homework

--- homework_northwind.sql
+++ homework_northwind.sql
@@ -4,12 +4,13 @@
 
 ---3.1. Посчитать количество заказов за все время. Смотри таблицу orders. Вывод: количество заказов.
 
-select count(*) "number of orders for all time" from public.orders
+select count(*) as "number of orders for all time" 
+from public.orders
 
 ---3.2. Посчитать сумму денег по всем заказам за все время (учитывая скидки).  Смотри таблицу order_details. Вывод: id заказа, итоговый чек ---(сумма стоимостей всех  продуктов со скидкой)
 
-
-
+select sum(cast((unit_price - (unit_price * discount)) * quantity as numeric(10,2))) as "sum of the prices of all discounted products"
+from public.order_details;
 
 
 ---3.3. Показать сколько сотрудников работает в каждом городе. Смотри таблицу employee. 
@@ -17,14 +18,28 @@ select count(*) "number of orders for all time" from public.orders
 --select city,  count(*) "number of employees" from public.employees
 --group by city;
 
-select distinct city, count(*) over(partition by city) "number of employees"
+select distinct city, count(*) over(partition by city) as "number of employees"
 from public.employees;
 
 ---3.4. Показать фио сотрудника (одна колонка) и сумму всех его заказов 
-select 
 
-
+select first_name || ' ' || last_name as "employee_name", sum(cost_orders) as "cost of all orders"
+from (
+	select orders.order_id, employee_id, 
+	sum(cast((order_details.unit_price - (order_details.unit_price * order_details.discount)) * order_details.quantity as numeric(10,2))) as cost_orders
+	from orders inner join order_details on orders.order_id = order_details.order_id
+	group by orders.order_id
+	order by orders.order_id) as t1 
+		inner join employees on t1.employee_id = employees.employee_id
+group by employee_name
+order by employee_name;
 
 
 ---3.5. Показать перечень товаров от самых продаваемых до самых непродаваемых (в штуках). - Вывести наименование продукта и количество проданных штук.
+
+select product_name as "name product", (select sum(quantity) from order_details where order_details.product_id = products.product_id) as "quantity"
+from products
+group by products.product_id
+order by quantity desc;
+
 
